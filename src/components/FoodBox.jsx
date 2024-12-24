444b0e828f033f3ed0d68e47aee1617f497ac44e
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-24 13:13:17 +0100

    iteration 2 | FoodBox component

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,30 @@
-// Your code here
+function FoodBox(props) {
+  // console.log(props);
+  const { foods } = props;
+  console.log(foods);
+
+  return (
+    <>
+      {foods.map((eachFood) => {
+        return (
+          <div>
+            <p>{eachFood.name}</p>
+
+            <img src={eachFood.image} />
+
+            <p>Calories: {eachFood.calories}</p>
+            <p>Servings {eachFood.servings}</p>
+
+            <p>
+              <b>Total Calories: {eachFood.servings * eachFood.calories} </b> kcal
+            </p>
+
+            <button>Delete</button>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default FoodBox;
